60b7d7372095384036d7cad63093f99703b1c5cb
Milana <milana.stojadinov@gmail.com>
2018-02-03 21:26:49 +0100

    Don't require css in main.js

--- src/components/Notebook.vue
+++ src/components/Notebook.vue
@@ -65,7 +65,7 @@ export default {
 };
 </script>
 
-<style scoped>
+<style>
 body {
   font-family: sans-serif;
   font-size: 16px;
--- src/main.js
+++ src/main.js
@@ -4,9 +4,6 @@ import Vue from 'vue';
 import App from './App';
 import router from './router';
 
-require('./assets/style.css');
-
-
 Vue.config.productionTip = false;
 
 /* eslint-disable no-new */
