806613c4032b96b05beee8ea9a4555617eaedf7a
Milana <milana.stojadinov@gmail.com>
2018-02-03 17:44:16 +0100

    Change landing page

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,15 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+  <Hello/>
   </div>
 </template>
 
 <script>
+import Hello from './components/Hello';
+
 export default {
   name: 'App',
+  components: { Hello },
 };
 </script>
 
